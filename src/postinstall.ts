5f06e526458aff135371dfc00471efa0a7a5a20d
ptvty <ptvty@proton.me>
2024-04-13 13:05:50 +0300

    Skip executable download if exists

--- src/postinstall.ts
+++ src/postinstall.ts
@@ -1,6 +1,6 @@
 import { get } from 'https';
 import { dirname } from 'path';
-import { createWriteStream, mkdirSync } from 'fs';
+import { createWriteStream, mkdirSync, existsSync, renameSync } from 'fs';
 import { EXECUTABLE_PATH, EXECUTABLE_URL } from './constants';
 
 function downloadStandaloneBinary() {
@@ -9,7 +9,12 @@ function downloadStandaloneBinary() {
     } catch (e) {
         // dir already exists (e.code === 'EEXIST')
     }
-    const file = createWriteStream(EXECUTABLE_PATH);
+    if (existsSync(EXECUTABLE_PATH)) {
+        console.log('Post-install: Download skipped, executable already exists.');
+        return;
+    }
+    const executableDownloadPartPath = `${EXECUTABLE_PATH}.part`;
+    const file = createWriteStream(executableDownloadPartPath);
     console.log(`Post-install: Downloading standalone binary... ${EXECUTABLE_URL}`);
     get(EXECUTABLE_URL, (response) => {
         const realBinaryUrl = response.headers?.location || EXECUTABLE_URL;
@@ -19,6 +24,7 @@ function downloadStandaloneBinary() {
             response.pipe(file);
             file.on("finish", () => {
                 file.close();
+                renameSync(executableDownloadPartPath, EXECUTABLE_PATH);
                 console.log('Post-install: Finished downloading standalone binary.');
             });
         });
